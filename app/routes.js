13bc7789f12e30032f8faad2c3f94c770c3a2be9
Jason <jiacan.h01@gmail.com>
2023-12-07 22:36:10 +0800

    Add router

---
+++ app/routes.js
@@ -0,0 +1,79 @@
+import { Router } from 'express';
+import { availability, ping } from './controllers.js';
+
+export const router = Router();
+
+/**
+ * @swagger
+ * /:
+ *  get:
+ *    summary: Returns the status of the API server.
+ *    responses:
+ *      200:
+ *        description: Server is up!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                status:
+ *                  type: string
+ *              example:
+ *                status: Healthy!
+ */
+router.get('/', ping);
+
+/**
+ * @swagger
+ * /{year}/{month}/{day}:
+ *  get:
+ *    summary: Retrieves places which are available on a given date.
+ *    description: Returns a list of available places on the given date.
+ *    parameters:
+ *      - in: path
+ *        name: year
+ *        required: true
+ *        description: Year in YYYY format
+ *        schema:
+ *          type: string
+ *          example: 2023
+ *      - in: path
+ *        name: month
+ *        required: true
+ *        description: Month in MM format
+ *        schema:
+ *          type: string
+ *          example: 12
+ *      - in: path
+ *        name: day
+ *        required: true
+ *        description: Day in DD format
+ *        schema:
+ *          type: string
+ *          example: '04'
+ *    responses:
+ *      200:
+ *        description: The list of available places.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                  type: string
+ *              example: [ Yonge, College ]
+ *      400:
+ *        description: 'Bad request: invalid params provided.'
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                cause:
+ *                  type: string
+ *              example:
+ *                message: Invalid date format!
+ *                cause: Invalid day!
+ */
+router.get('/:year/:month/:day', availability);
